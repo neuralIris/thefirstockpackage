2df8ffa80cfe98d50d30c82ba7160af5e1eae12d
Neuraliris13 <96634799+neuralIris@users.noreply.github.com>
2022-04-26 19:49:35 +0530

    Update login.py

--- FirstockAPI/login.py
+++ FirstockAPI/login.py
@@ -1,11 +1,11 @@
 from thefirstock import thefirstock
 
 login = thefirstock.firstock_login(
-                uid='SR1598',
-                pwd='Option@123',
-                factor2='15081998',
-                vc='SR1598_U',
-                appkey='ncoAPIkey27122021TGDYH1S',
+                uid='',
+                pwd='',
+                factor2='',
+                vc='',
+                appkey='',
 )
 
 print(login)
